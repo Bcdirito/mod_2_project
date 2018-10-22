46d58466e95b28a3cacc230c2a11887147492ef0
rajsrai <40576284+rajsrai@users.noreply.github.com>
2018-10-22 17:30:35 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :musicians, through: :reviews
+  has_secure_password
+
+  def new
+  end
+
+def create
+  user = User.new(user_params)
+  if user.save
+    session[:user_id] = user.id
+    redirect_to '/'
+  else
+    redirect_to '/signup'
+  end
+end
+
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :user_name, :password, :password_confirmation)
+end
 end
