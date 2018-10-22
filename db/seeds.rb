5afafd38aea11dbaa628ae282f794b38ecbeda82
Brian DiRito <bdirito@gmail.com>
2018-10-22 14:30:09 -0400

    seeded genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create(name: "Pop")
+Genre.create(name: "Rock")
+Genre.create(name: "R&B")
+Genre.create(name: "Rap")
+Genre.create(name: "Hip Hop")
+Genre.create(name: "Country")
+Genre.create(name: "Alternative")
+Genre.create(name: "EDM")
+Genre.create(name: "Folk")
+Genre.create(name: "Singer/Songwriter")
+Genre.create(name: "Funk")
+Genre.create(name: "Metal")
+Genre.create(name: "Adult Contemporary")
+Genre.create(name: "Other")
