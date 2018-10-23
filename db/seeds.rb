3adc4318724a5a5dcb29419b43fc87f3eab9ed07
Brian DiRito <40340183+Bcdirito@users.noreply.github.com>
2018-10-22 21:02:06 -0400

    Update seeds.rb
    "Added more seeds to test the view pages"

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,4 +25,22 @@ Genre.create(name: "Metal")
 Genre.create(name: "Adult Contemporary")
 Genre.create(name: "Other")
 
+User.create(first_name: "Brian", last_name: "DiRito", password: "password", password_digest: "password" )
+User.create(first_name: "Bijon", last_name: "Miles", password: "password", password_digest: "password" )
+User.create(first_name: "Rajvir", last_name: "Rai", password: "password", password_digest: "password" )
+User.create(first_name: "Prince", last_name: "Wilson", password: "password", password_digest: "password" )
+User.create(first_name: "Arren", last_name: "Alexander", password: "password", password_digest: "password" )
+
+Musician.create(name: "Brian Detlefs", genre: "Singer/Songwriter", band_members: nil, bio: "Singer/Songwriter from Seattle. Loves Prince." image: nil rate: 0)
+Musician.create(name: "Anthony Pagano", genre: "Singer/Songwriter", band_members: nil, bio: "Singer/Songwriter from Florida. Loves John Mayer" image: nil rate: 4)
+Musician.create(name: "Kyle Motsinger", genre: ["Pop", "Alternative"], band_members: nil, bio: "Singer/Songwriter from Illinois. Loves Scissor Sisters." image: nil rate: 3.5)
+Musician.create(name: "Catherine Greenfield", genre: "Folk", band_members: nil, bio: "Singer/Songwriter from Seattle. Loves Folk." image: nil rate: 2 )
+Musician.create(name: "John Arthur Greene", genre: ["Pop", "Rock"] band_members: nil, bio: "Singer/Songwriter from Seattle. Loves Prince." image: nil rate: 5)
+
+Review.create(user_id: 1, musician_id: 2, rating: 4, content: "KILLLLLLLLLLEEEEEEEEDDDDDDD ITTTTTTTTT")
+Review.create(user_id: 2, musician_id: 1, rating: 5, content: "KILLLLLLLLLLEEEEEEEEDDDDDDD ITTTTTTTTT")
+Review.create(user_id: 2, musician_id: 2, rating: 5, content: "KILLLLLLLLLLEEEEEEEEDDDDDDD ITTTTTTTTT")
+Review.create(user_id: 5, musician_id: 4, rating: 2, content: "KILLLLLLLLLLEEEEEEEEDDDDDDD ITTTTTTTTT")
+Review.create(user_id: 4, musician_id: 3, rating: 1, content: "KILLLLLLLLLLEEEEEEEEDDDDDDD ITTTTTTTTT")
+
 puts "Done!"
